93f4e8da722089454e40cd8134eb01b0e5429610
Wassim01-3 <wassimmars0@gmail.com>
2026-08-06 15:21:21 +0100

    Add external dependency for push notifications

--- vite.config.ts
+++ vite.config.ts
@@ -133,6 +133,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      external: ['@capacitor/push-notifications'],
       output: {
         manualChunks: (id) => {
           if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/') || id.includes('node_modules/react-router-dom/')) {
